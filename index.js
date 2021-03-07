06bbbb36aa7fbc77cba7c353de04b1eef91d9d4a
DevMcCoy <devmccoy@devmccoys-mbp.home>
2021-03-07 13:22:31 -0500

    adding a json call

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const members = [
+    {
+        id: 1, 
+        name: 'John Doe',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        status: 'active'
+    }
+]
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
 // to create a route:
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!!</h1>')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+
+// set a static folder
+// use is the method that we use to include middleware
+app.use(express.static(path.join(__dirname, 'public')));
+// here public is set as the static folder
 
 
 const PORT = process.env.PORT || 5000;
